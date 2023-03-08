af14dec59bf909442e1f2fa9dab395a5cbbbbbac
Sosocwala <Sonwabilcwala6@gmail.com>
2023-03-08 12:32:23 +0200

    First commit in python

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+print(1 + 1)        #Addition(+)
+print(10 * 2)       #Multiplicatin(*)   
+print(15 - 5)       #subtraction(-)
+print(40 / 4)       #division(/)
+print(10 ** 2)      #exponential(**)
+print(30 // 5)      #Floor division operater(//)
+print(20 % 4)       #modulus(%)
+
+#checking data types
+
+print(type(10))         #int
+print(type(34.33))      #float    
+print(type('Asanda'))   #string
+print(type(2 + 4j))     #complex
+print(type([20.3, 50.5, 'asanda'])) #list
+print(type({'first_name':'Asanda'}))#Dictionary
+print(type({20.25, 0.55, 55})) #Set
+print(type((20.22, 10.14, 8.54))) #Tuple
+
